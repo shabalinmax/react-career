989121cdbdc2dcca65d546424adb190db974c58f
Max Shabalin <shabalinmaksimromanovich@gmail.com>
2022-10-04 21:07:42 +0300

    reworked rating system, updated rating page

--- src/pages/companies/index.jsx
+++ src/pages/companies/index.jsx
@@ -5,7 +5,6 @@ const Companies = ({vacancyList}) => {
         <div className={'companiesWrapper'}>
             <div className="companies">
                 <div className={'temporaryInfo'}>
-
                 </div>
                 {vacancyList.map(company =>
                 <div
@@ -16,14 +15,47 @@ const Companies = ({vacancyList}) => {
                             {company.companyName}
                         </div>
                         <div className="companyRating">
-                            {company.rating !== 0 ? <div> средняя оценка компании: {company.rating} </div> : <div>у этой компании еще не отзывов :(</div>  }
+                            {company.rating.length !== 0 ?
+                                <div>средняя оценка компании:
+                                    {(company.rating.reduce((partialSum, a) => partialSum + a, 0)/company.rating.length).toFixed(2)} </div>
+                                :
+                                <div>у этой компании еще не отзывов :(</div> }
 
                         </div>
                     </div>
-
                 </div>
                 )}
             </div>
+            <div className="rateCompany">
+                <h1 >
+                    Если вы сталкивались с работой в одной из следующих фирм, можете оставить свой балл
+                </h1>
+                <div className="rateSection">
+                    <select onChange={event => console.log(event.target.value)} className={'companySelect'}>
+                        {vacancyList.map(el => <option key={el.id} value={el.companyName}>{el.companyName}</option>)}
+                    </select>
+                    <select onChange={event => console.log(event.target.value)} className={'ratingSelect'}>
+                        <option value={5}>
+                            5
+                        </option>
+                        <option value={4}>
+                            4
+                        </option>
+                        <option value={3}>
+                            3
+                        </option>
+                        <option value={2}>
+                            2
+                        </option>
+                        <option value={1}>
+                            1
+                        </option>
+                    </select>
+                    <button>Отправить</button>
+                </div>
+            </div>
+
+
         </div>
     );
 };
